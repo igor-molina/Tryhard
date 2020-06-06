69803af1a155900b8d361422b23d81c9d0e5d1e3
Igor <igordsmolina@gmail.com>
2020-06-05 23:27:23 -0300

    Criando menu lateral junto com ícones
    Nesta atualização, adiciono um menu lateral, com ícones para ajudar na interface.

--- Home/home_page.php
+++ Home/home_page.php
@@ -3,13 +3,68 @@
     include "../Database/conection.php";
 ?>
 
+<!doctype html>
 <html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home_page.css">
     <title>Tryhard</title>
 </head>
 <body>
-    <h1>Página principal</h1>
+<div class="primary-nav">
+
+	<nav role="navigation" class="menu">
+
+		<img src="images/chest.png" class="inventory">
+
+		<img src="images/pergaminho.png" class="create-session">
+
+		<img src="images/book.png" class="adventure-books">
+
+		<img src="images/adicionar.png" class="add-alies">
+
+		<div class="overflow-container">
+
+			<ul class="menu-dropdown">
+
+				<li><a href="#">Inventário</a></li>
+
+				<li class="menu-hasdropdown">
+					<a href="#">Crie sua campanha</a>
+					<input type="checkbox" class="sub-menu-checkbox" id="settings" />
+
+					<ul class="sub-menu-dropdown">
+						<li><a href="">Profile</a></li>
+						<li><a href="">Security</a></li>
+						<li><a href="">Account</a></li>
+					</ul>
+				</li>
+
+				<li><a href="#">Livros</a></li>
+
+				<li><a href="#">Recrute um aliado</a></li>
+
+			</ul>
+
+		</div>
+
+	</nav>
+
+</div>
+
+<div class="new-wrapper">
+
+	<div id="main">
+
+		<div id="main-contents">
+
+			<h1>Página principal</h1>
+
+		</div>
+
+	</div>
+
+</div>
 </body>
 </html>
--- Login/functions_login.php
+++ Login/functions_login.php
@@ -4,7 +4,7 @@
         $dayWeek = array(0 => "Domingo", 1 => "Segunda-feira", 2 => "Terça-feira", 3 => "Quarta-feira", 4 => "Quinta-feira", 5 => "Sexta-feira", 6 => "Sábado");
         $month = array(1 => "Janeiro",2 => "Fevereiro",3 => "Março",4 => "Abril",5 => "Maio",6 => "Junho",7 => "Julho",8 => "Agosto",9 => "Setembro",10 => "Outubro",11 => "Novembro",12 => "Dezembro");
 
-        $valor = $dayWeek[date('w')].", ".date('d')." de ".$month[date('n')]." de ".date('y');
+        $valor = $dayWeek[date('w')].", ".date('d')." de ".$month[date('n')]." de ".date('yy');
 
         return $valor;
     }
